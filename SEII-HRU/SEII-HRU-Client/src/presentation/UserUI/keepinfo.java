7eb7e757c523ac85f31d98e48880bd024403450f
1PLee <1017270947@qq.com>
2016-10-17 23:17:17 +0800

    keepinfo

---
+++ SEII-HRU/SEII-HRU-Client/src/presentation/UserUI/keepinfo.java
@@ -0,0 +1,16 @@
+package presentation.UserUI;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class keepinfo extends Application {
+
+	@Override
+	public void start(Stage primaryStage) {
+		
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
