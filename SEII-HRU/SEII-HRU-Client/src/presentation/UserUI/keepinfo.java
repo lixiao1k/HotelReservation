ccf3841b5dcea3ea030b5729281e40f3074f73f2
1PLee <1017270947@qq.com>
2016-10-18 23:35:56 +0800

    客户界面

--- SEII-HRU/SEII-HRU-Client/src/presentation/UserUI/keepinfo.java
+++ SEII-HRU/SEII-HRU-Client/src/presentation/UserUI/keepinfo.java
@@ -39,11 +39,27 @@ public GridPane addGridPane(){
 	grid.setHgap(10);
 	grid.setPadding(new Insets(10,10,0,10));
 	
-	Text ID=new Text("用户ID");
+	Text ID=new Text("用户ID:");
 	grid.add(ID, 0, 0);
-	TextField idfield=new TextField("XXX");
+	TextField idfield=new TextField();
 	grid.add(idfield, 1, 0,2,1);
-	
+	Text Phone=new Text("联系方式:");
+	grid.add(Phone,0,1);
+	TextField phonefield=new TextField();
+	grid.add(phonefield, 1,1,2,1);
+	Text credit=new Text("信用值:");
+	grid.add(credit, 0, 2);
+	TextField creditfield=new TextField();
+	grid.add(creditfield, 1, 2,2,1);
+	Text compony=new Text("企业名称:");
+	grid.add(compony, 0, 3);
+	TextField componyfield=new TextField();
+	grid.add(componyfield, 1, 3,2,1);
+	Text birth=new Text("生日:");
+	grid.add(birth, 0, 4);
+	TextField birthfield=new TextField();
+	birthfield.setPromptText("仅限个人会员");
+	grid.add(birthfield, 1, 4,2,1);
 	return grid;
 }
 	public static void main(String[] args) {
