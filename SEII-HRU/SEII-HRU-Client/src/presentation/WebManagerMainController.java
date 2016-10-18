1bb14449c9c7016e60ae20a544272f44d322302d
SteinsKurisu <961564618@qq.com>
2016-10-18 10:29:20 +0800

    Revert "Revert "现有所有主界面跳转分界面补全""
    This reverts commit 7b7965803a00f96bb81d8ca9e41d7d3bc31865fd.

---
+++ SEII-HRU/SEII-HRU-Client/src/presentation/WebManagerMainController.java
@@ -0,0 +1,33 @@
+package presentation;
+
+import java.io.IOException;
+
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.layout.GridPane;
+
+public class WebManagerMainController {
+	@FXML private GridPane hotelmain;
+	@FXML protected void goSetHotelInfo(ActionEvent event){
+		try {
+			Parent HotelInfo = FXMLLoader.load(getClass().getResource("HotelUI/SetHotelInfo.fxml"));
+			HotelInfo.getProperties().put("NAME", "HotelInfo");
+			ObservableList<Node> list = hotelmain.getChildren();
+			for (Node node:list){
+				String value = (String) node.getProperties().get("NAME");
+				if (value!=null&&(value.contains("List")||value.contains("Info"))){
+					list.remove(node);
+					break;
+				}
+			}
+			hotelmain.add(HotelInfo, 2, 1);
+		} catch (IOException e) {
+			// log ÈÕÖ¾&&×´Ì¬À¸
+		}
+		
+	}
+}
--- SEII-HRU/SEII-HRU-Client/src/presentation/WebSalerMainUI.java
+++ SEII-HRU/SEII-HRU-Client/src/presentation/WebSalerMainUI.java
@@ -16,10 +16,10 @@ public class WebSalerMainUI extends Application{
 	public void start(Stage primaryStage) throws Exception {
 		// TODO Auto-generated method stub
 		Parent root = FXMLLoader.load(getClass().getResource("WebSalerMainUI.fxml"));
-		Parent HotelInfo = FXMLLoader.load(getClass().getResource("StrategyUI/CreateNewStrategy.fxml"));
-		HotelInfo.getProperties().put("NAME", "HotelInfo");
+		Parent NewStrategy = FXMLLoader.load(getClass().getResource("StrategyUI/CreateNewStrategy.fxml"));
+		NewStrategy.getProperties().put("NAME", "NewStrategy");
 		GridPane pane = (GridPane) root.lookup("#hotelmain");
-		pane.add(HotelInfo, 2, 1);
+		pane.add(NewStrategy, 2, 1);
 		Scene scene = new Scene(root,900,600);
 		primaryStage.setScene(scene);
 		primaryStage.show();
