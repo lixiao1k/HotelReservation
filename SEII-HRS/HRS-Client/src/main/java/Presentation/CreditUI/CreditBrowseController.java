1316b8007c0d5403a82056174a775e1a689ff6a9
李晓冬 <lixiao1k@163.com>
2016-12-09 20:23:01 +0800

    浏览信用记录

--- SEII-HRS/HRS-Client/src/main/java/Presentation/CreditUI/CreditBrowseController.java
+++ SEII-HRS/HRS-Client/src/main/java/Presentation/CreditUI/CreditBrowseController.java
@@ -2,9 +2,12 @@ package Presentation.CreditUI;
 
 import java.io.IOException;
 import java.net.URL;
+import java.time.LocalDate;
+import java.util.Iterator;
 import java.util.ResourceBundle;
 
 import Presentation.MemberUI.KeepPersonInfoController;
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -13,8 +16,11 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
 import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 import vo.CreditVO;
 
 public class CreditBrowseController implements Initializable{
@@ -22,9 +28,10 @@ public class CreditBrowseController implements Initializable{
 	@FXML Label nameLabel;
 	@FXML Label creditLabel;
 	@FXML ListView<CreditVO> creditListView;
-	private KeepPersonInfoController keeppersoninfocontroller;
-	private String username;
-	private long userid;
+	KeepPersonInfoController keeppersoninfocontroller;
+	String username;
+	long userid;
+	ObservableList<CreditVO> creditListViewData;
 	@FXML
 	protected void goBack(ActionEvent e){
 		GridPane clientmain=(GridPane)nameLabel.getScene().getWindow().getScene().getRoot();
@@ -52,10 +59,55 @@ public class CreditBrowseController implements Initializable{
 	public void setBaseInfo(long userid){
 		this.userid=userid;
 	}
-	
+	private void initialListViewData(Iterator<CreditVO> iterator){
+		while(iterator.hasNext()){
+			creditListViewData.add(iterator.next());
+		}
+	}
 	public void initialize(URL location, ResourceBundle resources) {
 		// TODO Auto-generated method stub
-		
+		creditListViewData=FXCollections.observableArrayList();
+	    LocalDate date=LocalDate.now();
+		CreditVO credit1=new CreditVO(151250084, date, 26, 398);
+		CreditVO credit2=new CreditVO(151250086, date, -26, 226);
+		creditListViewData.add(credit1);
+		creditListViewData.add(credit2);
+		creditListView.setCellFactory(e->new CreditListCell());
+		creditListView.setItems(creditListViewData);
+	}
+	class CreditListCell extends ListCell<CreditVO>{
+		public void updateItem(CreditVO item,boolean empty){
+			super.updateItem(item, empty);
+			if(item!=null){
+				GridPane cell=new GridPane();
+				Label id=new Label("ID:"+Long.toString(item.getUserId()));
+				id.setFont(new Font("KaiTi_GB2312",15));
+				id.setPrefWidth(150);
+				id.setPrefHeight(50);
+				Label time=new Label(item.getDate().toString());
+				time.setFont(new Font("KaiTi_GB2312",15));
+				Label delta=new Label(Integer.toString(item.getDelta()));
+				delta.setFont(new Font("KaiTi_GB2312", 20));
+				delta.setPrefWidth(100);
+				if(item.getDelta()>0){
+					delta.setTextFill(Color.GREEN);
+				}else if(item.getDelta()<0){
+					delta.setTextFill(Color.RED);
+				}else{
+					delta.setTextFill(Color.BLACK);
+				}
+				Label credit=new Label("信用总值:"+Integer.toString(item.getCredit()));
+				credit.setFont(new Font("KaiTi_GB2312",20));
+				credit.setPrefWidth(200);
+				cell.add(id, 0, 0);
+				cell.add(time,3,0);
+				cell.add(delta, 1, 0);
+				cell.add(credit, 2, 0);
+				setGraphic(cell);
+			}else{
+				setGraphic(null);
+			}
+		}
 	}
 
 }
