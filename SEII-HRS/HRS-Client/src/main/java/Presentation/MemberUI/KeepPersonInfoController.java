f8c88608a5f1d1d6b24a98ffaab743e15cf4aa2f
SteinsKurisu <961564618@qq.com>
2016-12-13 19:09:38 +0800

    update

--- SEII-HRS/HRS-Client/src/main/java/Presentation/MemberUI/KeepPersonInfoController.java
+++ SEII-HRS/HRS-Client/src/main/java/Presentation/MemberUI/KeepPersonInfoController.java
@@ -23,7 +23,6 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.ComboBox;
-import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
