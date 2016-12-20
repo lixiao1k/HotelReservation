d00849239ef2c44e827ee3e41d04d29989198534
1PLee <1017270947@qq.com>
2016-12-20 22:31:18 +0800

    搜索酒店

--- SEII-HRS/HRS-Client/src/main/java/Presentation/HotelUI/HotelBrowseController.java
+++ SEII-HRS/HRS-Client/src/main/java/Presentation/HotelUI/HotelBrowseController.java
@@ -7,18 +7,26 @@ import java.time.Instant;
 import java.time.LocalDate;
 import java.time.ZoneId;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.ResourceBundle;
 import java.util.Set;
+import java.util.Map;
+import java.util.Map.Entry;
+
 
 import Presentation.CreditUI.CreditBrowseController;
 import datacontroller.DataController;
 import info.BusinessCircle;
 import info.BusinessCity;
 import info.ListWrapper;
+import info.Rank;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
@@ -50,11 +58,14 @@ import logic.service.ServiceFactory;
 import rmi.RemoteHelper;
 import vo.BasicHotelVO;
 import vo.HotelItemVO;
+import vo.OrderVO;
 import vo.SearchHotelVO;
 
 import org.controlsfx.control.CheckComboBox;
 import org.controlsfx.control.Notifications;
 import org.controlsfx.control.PopOver;
+import org.controlsfx.control.Rating;
+
 
 
 public class HotelBrowseController implements Initializable{
@@ -89,24 +100,193 @@ public class HotelBrowseController implements Initializable{
 	public void search(ActionEvent e)
 	{
 		String searchinfo=searchField.getText();
+		String[] aimhotel=searchinfo.split("");
+		Map<BasicHotelVO,Integer>map=new HashMap<>();
+		List<BasicHotelVO> hotelstextlimit = new ArrayList<BasicHotelVO>();
+		try {
+			Iterator<BasicHotelVO> it=basicHotel.iterator();
+			
+			while(it.hasNext())
+			{
+				BasicHotelVO  bihvo = it.next();
+				int count=0;
+				for(int i=0;i<aimhotel.length;i++)
+				{
+					if(bihvo.getHotelName().contains(aimhotel[i]))
+					{
+						count++;
+					}
+					
+				}
+			
+				map.put(bihvo, count);
+				
+			}
+			map = sortMapByValue(map);
+		    for (Map.Entry<BasicHotelVO, Integer> entry : map.entrySet()) {
+		    	hotelstextlimit.add(entry.getKey());
+		    }
+			hotelListViewData=FXCollections.observableArrayList(hotelstextlimit);
+
+			show();
 		
 		
+		} catch (RemoteException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+	
+
 		
 	}
 	
+	public void show()
+	{
+		hotelListView.setCellFactory(e->new hotelListCell());
+		hotelListView.setItems(hotelListViewData);
+	}
+	
+	  private Map<BasicHotelVO,Integer> sortMapByValue(Map<BasicHotelVO, Integer> map) {
+	        List<Map.Entry<BasicHotelVO, Integer>> mapList = new ArrayList<Map.Entry<BasicHotelVO, Integer>>(
+	                map.entrySet());
+	        Collections.sort(mapList, new Comparator<Map.Entry<BasicHotelVO, Integer>>() {
+	            @Override
+	            public int compare(Entry<BasicHotelVO, Integer> o1,
+	                    Entry<BasicHotelVO, Integer> o2) {
+	                return -1*(o1.getValue()-o2.getValue());
+	            }
+	        });
+	        Map<BasicHotelVO,Integer> result = new LinkedHashMap<BasicHotelVO,Integer>();
+	        for(Map.Entry<BasicHotelVO, Integer> entry:mapList){
+	            result.put(entry.getKey(), entry.getValue());
+	        }
+	        return result;
+	    }
 	
 	//用组合限制条件搜索
 	public void searchInChoice(String value)
 	{
-		if(hotelListViewData==null)
+		if(hotelListViewData==null){
+			System.out.println("datakong");
 			return;
+		}
+		
 		if(value==null)
 			return;
 		else
 		{
 			String star=limitStar.getSelectionModel().getSelectedItem();
+			
 			String rank=limitRank.getSelectionModel().getSelectedItem();
+		
 			String price=limitPrice.getSelectionModel().getSelectedItem();
+	
+			Rank carank=Rank.NONE;
+			switch(star)
+			{
+			case"一星级":{
+				carank=Rank.ONE;break;
+			}
+			case"二星级":{
+				carank=Rank.TWO;break;
+			}
+			case"三星级":{
+				carank=Rank.THREE;break;
+			}
+			case"四星级":{
+				carank=Rank.FOUR;break;
+			}
+			case"五星级":{
+				carank=Rank.FIVE;break;
+			}
+			case"无":
+			{
+				carank=Rank.NONE;break;
+			}
+			}
+			double lscore=0;//记录最低评分
+			double hscore=0;//记录最高评分
+			switch(rank)
+			{
+			case"4.0~5.0":{
+				lscore=4.0;
+				hscore=5.0;
+				break;
+			}
+			case"3.0~4.0":{
+				lscore=3.0;
+				hscore=4.0;
+				break;
+			}
+			case"3.0以下":{
+				lscore=0;
+				hscore=3.0;
+			}
+			}
+		
+			
+			double lprice=0;
+			double hprice=10000;
+			switch(price)
+			{
+			case"200元以下":{
+				lprice=0;
+				hprice=200;
+				break;
+			}
+			case"200~500":{
+				lprice=200;
+				hprice=500;
+				break;
+			}
+			case"500元以上":{
+				lprice=500;
+				hprice=100000;
+				break;
+			}
+			}
+			
+			
+			try {
+				
+				Iterator<BasicHotelVO> it=basicHotel.iterator();
+				List<BasicHotelVO> hotelslimit = new ArrayList<BasicHotelVO>();
+				while(it.hasNext()){
+					BasicHotelVO  bihvo = it.next();
+					if(bihvo.getRank()==carank&&bihvo.getScore()>lscore&&bihvo.getScore()<hscore)
+					{
+						double hotelleast=100000;//记录酒店最低价格
+				         Set<HotelItemVO> hotIt=bihvo.getRooms();
+			                
+			                for(HotelItemVO  htlVO:hotIt)
+			                {
+			                	if(htlVO.getPrice()<hotelleast)
+			                	{
+			                		hotelleast=htlVO.getPrice();
+			                		
+			                	}
+			                }
+			             //   System.out.println(hotelleast);
+			                if(hotelleast>lprice&&hotelleast<hprice)
+			                {
+			                	hotelslimit.add(bihvo);
+			                }
+					}
+					
+				}
+	
+				System.out.println(hotelslimit.size());
+				hotelListViewData=FXCollections.observableArrayList(hotelslimit);
+				hotelListView.setCellFactory(e->new hotelListCell());
+				hotelListView.setItems(hotelListViewData);
+				
+
+				
+			} catch (RemoteException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+	
 			
 		}
 		
@@ -119,6 +299,7 @@ public class HotelBrowseController implements Initializable{
 		    	{
 		    		System.out.println("vo有问题");
 		    	}
+		   // 	System.out.println(vo.getBusinessCircle().getName());
 				basicHotel=	hotelbl.getHotels(vo);//根据searchvo返回搜索到的酒店信息
 				List<BasicHotelVO> hotels = new ArrayList<BasicHotelVO>();
 				System.out.println(basicHotel.size());
@@ -127,7 +308,7 @@ public class HotelBrowseController implements Initializable{
 					BasicHotelVO  bihvo = it.next();
 					hotels.add(bihvo);
 				}
-				System.out.println("ssss");
+			//	System.out.println("ssss");
 				hotelListViewData=FXCollections.observableArrayList(hotels);
 				hotelListView.setCellFactory(e->new hotelListCell());
 				hotelListView.setItems(hotelListViewData);
@@ -178,8 +359,36 @@ public class HotelBrowseController implements Initializable{
 	                cell.prefWidthProperty().bind(hotelListView.widthProperty().subtract(2));
 	                Label hotelName = new Label(item.getHotelName()+"/"+item.getScore());//酒店名称+评分
 	                hotelName.setFont(new Font("YouYuan",20));
-	                Label star=new Label(item.getRank().toString());
-	                star.setFont(new Font("YouYuan",20));
+	            //    Label star=new Label(item.getRank().toString());
+	            //    star.setFont(new Font("YouYuan",20));
+	                int star1=0;
+	                if(item.getRank()==Rank.NONE)
+	                {
+	                	star1=0;
+	                }
+	                else if(item.getRank()==Rank.ONE)
+	                {
+	                	star1=1;
+	                }
+	                else if(item.getRank()==Rank.TWO)
+	                {
+	                	star1=2;
+	                }
+	                else if(item.getRank()==Rank.THREE)
+	                {
+	                	star1=3;
+	                }
+	                else if(item.getRank()==Rank.FOUR)
+	                {
+	                	star1=4;
+	                }
+	                else if(item.getRank()==Rank.FIVE)
+	                {
+	                	star1=5;
+	                }
+	                Rating star=new Rating(5,star1);
+	                star.setPartialRating(false);
+	                star.setUpdateOnHover(false);
 	                long theHotelID=item.getHotelId();//得到ID
 	                boolean flag=false;
 	                try {
@@ -209,7 +418,7 @@ public class HotelBrowseController implements Initializable{
 	                }
 	                else
 	                {
-	                	history.setText("    ");
+	                	history.setText("未预订");
 	                	
 	                }
 	                Set<HotelItemVO> hotIt=item.getRooms();
@@ -239,17 +448,22 @@ public class HotelBrowseController implements Initializable{
 	                
 	                //差setmagin
 	                cell.setHalignment(createOrder, HPos.RIGHT);
+	                
 	                cell.add(hotelName, 0, 0);
 	                cell.add(star, 1, 0);
+	                cell.setMargin(star, new Insets(0,0,0,20));
 	                cell.add(history, 2, 0);
+	                cell.setMargin(history, new Insets(0,0,0,30));
 	                cell.add(least, 3, 0);
+	                cell.setMargin(least, new Insets(0,0,0,30));
 	                cell.add(createOrder, 4, 0);
+	                cell.setMargin(createOrder, new Insets(0,0,0,70));
 	                setGraphic(cell);
 				}
 				else
 				{
 					setGraphic(null);
-					System.out.println("meiyou");
+				//	System.out.println("meiyou");
 				}
 			}
 		
@@ -355,7 +569,7 @@ public class HotelBrowseController implements Initializable{
 		if(circleBox.getSelectionModel().getSelectedItem()==null)
 		{
 
-			System.out.println("啥也没有");
+		//	System.out.println("啥也没有");
 
 		}
 		else
@@ -438,11 +652,14 @@ public class HotelBrowseController implements Initializable{
 			mainPane.add(limitBox, 0, 0);
 			mainPane.setMargin(limitBox, new Insets(150,0,0,150));*/
 			limitStarList=FXCollections.observableArrayList();
-			limitStarList.add("三星级(及以下)");
+			limitStarList.add("无");
+			limitStarList.add("一星级");
+			limitStarList.add("二星级");
+			limitStarList.add("三星级");
 			limitStarList.add("四星级");
 			limitStarList.add("五星级");
 			limitStar.setItems(limitStarList);
-			limitStar.setValue("三星级(及以下)");
+			limitStar.setValue("三星级");
 			
 			limitRankList=FXCollections.observableArrayList();
 			limitRankList.add("4.0~5.0");
